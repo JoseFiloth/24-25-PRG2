fe5808d0eda2b7276d791e1d4d008b80d99e53d5
JoseFiloth <jose.filoth@alumnos.uneatlantico.es>
2025-03-18 13:56:16 +0100

    Triki

---
+++ entregas/filothJose/Reto001/pruebas/Tablero.java
@@ -0,0 +1,26 @@
+package entregas.filothJose.Reto001.pruebas;
+
+public class Tablero {
+
+   private char [][] casillas;
+
+   public Tablero(){
+ 
+   casillas = new char[3][3];
+   for(int i = 0; i < )  
+
+   }
+   
+    public void mostrar() {
+      for(int i = 0; i < casillas.length; i++){}
+    }
+
+    public boolean hayTresEnRaya() {
+        
+    }
+
+    public boolean estaCompleto(Jugador jugador) {
+       
+    }
+
+}
